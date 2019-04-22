7557994d4072247941b1fc65e6e654f72ba11f0e
timurista <timothy.urista@gmail.com>
2019-04-22 06:35:34 -0700

    add main functions

---
+++ main/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	// "database/sql"
+	// "encoding/json"
+	"fmt"
+	// "log"
+	// "net/http"
+	// "strings"
+	//
+	// jwt "github.com/dgrijalva/jwt-go"
+	// "github.com/gorilla/mux"
+	// "github.com/lib/pq"
+	// "golang.org/x/crypto/bcrypt"
+)
+
+func main() {
+	fmt.Printf("hello, world\n")
+}
