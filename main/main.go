a046afa97ecd63e98454364bab2be974970e0450
Tim Urista <timothy.urista@gmail.com>
2019-04-23 21:52:38 -0700

    setup the auth handlers for signup and login

--- main/main.go
+++ main/main.go
@@ -4,16 +4,43 @@ import (
 	// "database/sql"
 	// "encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+
 	// "log"
 	// "net/http"
 	// "strings"
 	//
 	// jwt "github.com/dgrijalva/jwt-go"
-	// "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 	// "github.com/lib/pq"
 	// "golang.org/x/crypto/bcrypt"
 )
 
 func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/signup", signup).Methods("POST")
+	router.HandleFunc("/login", login).Methods("POST")
+	router.HandleFunc("/proected", TokenVerifyMiddleWare(protectedEndpoint)).Methods("POST")
+
 	fmt.Printf("hello, world\n")
+	log.Println("Listening on port 8000...")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("signup\n")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("login\n")
+}
+
+func protectedEndpoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("protectedEndpoint\n")
+}
+
+func TokenVerifyMiddleWare(next http.HandlerFunc) http.HandlerFunc {
+	fmt.Printf("TokenVerifyMiddleWare\n")
+	return nil
 }
