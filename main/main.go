d6dff3c3264da9efee64a97ed4bd6077dcd63980
timurista <timothy.urista@gmail.com>
2019-04-24 07:27:51 -0700

    add endponits login

--- main/main.go
+++ main/main.go
@@ -21,19 +21,20 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/signup", signup).Methods("POST")
 	router.HandleFunc("/login", login).Methods("POST")
-	router.HandleFunc("/proected", TokenVerifyMiddleWare(protectedEndpoint)).Methods("POST")
+	router.HandleFunc("/protected", TokenVerifyMiddleWare(protectedEndpoint)).Methods("GET")
 
-	fmt.Printf("hello, world\n")
+	// fmt.Printf("hello, world\n")
 	log.Println("Listening on port 8000...")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
 func signup(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("signup\n")
+	// fmt.Printf("signup\n")
+	w.Write([]byte("successfully called signup"))
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("login\n")
+	w.Write([]byte("successfully called login"))
 }
 
 func protectedEndpoint(w http.ResponseWriter, r *http.Request) {
