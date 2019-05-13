e86272d72ed1c2da0e0459c4c971bfc23f61aba6
timurista <timothy.urista@gmail.com>
2019-05-13 06:14:56 -0700

    add main go and docker file update

--- main.go
+++ main.go
@@ -1,209 +1,40 @@
 package main
 
 import (
-	"database/sql"
-	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
-	"strings"
 
-	"github.com/timurista/go-auth-api/driver"
-	"github.com/timurista/go-auth-api/models"
-
-	"github.com/davecgh/go-spew/spew"
-	"github.com/dgrijalva/jwt-go"
-	"github.com/gorilla/mux"
-	"github.com/subosito/gotenv"
-	"golang.org/x/crypto/bcrypt"
+	"go-auth-api/api"
 )
 
-var db *sql.DB
-
-func init() {
-	gotenv.Load()
-}
-
 func main() {
-	db = driver.ConnectDB()
+	http.HandleFunc("/", index)
+	http.HandleFunc("/api/echo", echo)
 
-	router := mux.NewRouter()
-	router.HandleFunc("/signup", signup).Methods("POST")
-	router.HandleFunc("/login", login).Methods("POST")
-	router.HandleFunc("/protected", TokenVerifyMiddleWare(protectedEndpoint)).Methods("GET")
+	http.HandleFunc("/api/books", api.BooksHandleFunc)
+	http.HandleFunc("/api/books/", api.BookHandleFunc)
 
-	// fmt.Printf("hello, world\n")
-	log.Println("Listening on port 8000...")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	fmt.Println("running on 8080")
+	http.ListenAndServe(port(), nil)
 }
 
-func respondWithError(w http.ResponseWriter, status int, data interface{}) {
-	w.WriteHeader(status)
-	json.NewEncoder(w).Encode(data)
+func echo(w http.ResponseWriter, r *http.Request) {
+	message := r.URL.Query()["message"][0]
+
+	w.Header().Add("Content-Type", "text/plain")
+	fmt.Fprintf(w, message)
 }
 
-func responseJSON(w http.ResponseWriter, data interface{}) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(data)
+func port() string {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	return ":" + port
 }
 
-func GenerateToken(user models.User) (string, error) {
-	secret := os.Getenv("SECRET")
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"email": user.Email,
-		"iss":   "course",
-	})
-	// spew.Dump(token)
-	tokenString, err := token.SignedString([]byte(secret))
-	if err != nil {
-		log.Fatal(err)
-	}
-	return tokenString, nil
-}
-
-func signup(w http.ResponseWriter, r *http.Request) {
-	var user models.User
-	var error models.Error
-
-	json.NewDecoder(r.Body).Decode(&user)
-
-	if user.Email == "" {
-		error.Message = "Email is missing"
-		respondWithError(w, http.StatusBadRequest, error)
-		return
-	}
-
-	if user.Password == "" {
-		error.Message = "Password is missing"
-		respondWithError(w, http.StatusBadRequest, error)
-		return
-	}
-
-	// password hashing / salting
-	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
-	// fmt.Println(user)
-	// fmt.Println("-------")
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	user.Password = string(hash)
-	spew.Dump(user)
-
-	stmt := "insert into users (email, password) values($1, $2) RETURNING id"
-	err = db.QueryRow(stmt, user.Email, user.Password).Scan(&user.ID)
-	if err != nil {
-		error.Message = "Server error"
-		respondWithError(w, http.StatusInternalServerError, error)
-		return
-	}
-	user.Password = ""
-
-	w.Header().Set("Content-Type", "application/json")
-	responseJSON(w, user)
-}
-
-func login(w http.ResponseWriter, r *http.Request) {
-
-	var user models.User
-	var jwt models.JWT
-	var error models.Error
-
-	json.NewDecoder(r.Body).Decode(&user)
-
-	spew.Dump(user)
-
-	if user.Email == "" {
-		error.Message = "Email is missing"
-		respondWithError(w, http.StatusBadRequest, error)
-		return
-	}
-
-	if user.Password == "" {
-		error.Message = "Password is missing"
-		respondWithError(w, http.StatusBadRequest, error)
-		return
-	}
-
-	password := user.Password // sent password by user
-	row := db.QueryRow("select * from users where email=$1", user.Email)
-	err := row.Scan(&user.ID, &user.Email, &user.Password)
-
-	fmt.Println("ROW", row)
-
-	if err != nil {
-		if err == sql.ErrNoRows {
-			error.Message = "The user does not exist"
-			respondWithError(w, http.StatusBadRequest, error)
-			return
-		} else {
-			log.Fatal(error)
-		}
-	}
-
-	spew.Dump(user)
-
-	hashedPassword := user.Password
-	err = bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
-
-	if err != nil {
-		error.Message = "Invalid Password"
-		respondWithError(w, http.StatusUnauthorized, error)
-		return
-	}
-
-	token, err := GenerateToken(user)
-	if err != nil {
-		log.Fatal(err)
-	}
-
+func index(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	jwt.Token = token
-
-	responseJSON(w, jwt)
-}
-
-func protectedEndpoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("protectedEndpoint\n")
-}
-
-func TokenVerifyMiddleWare(next http.HandlerFunc) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		var errorObject models.Error
-		authHeader := r.Header.Get("Authorization")
-		bearerToken := strings.Split(authHeader, " ")
-		// fmt.Println(bearerToken)
-
-		if len(bearerToken) == 2 {
-			authToken := bearerToken[1]
-			token, error := jwt.Parse(authToken, func(token *jwt.Token) (interface{}, error) {
-				// spew.Dump(token)
-				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-					return nil, fmt.Errorf("There was an error")
-				}
-				return []byte(os.Getenv("SECRET")), nil
-			})
-
-			if error != nil {
-				errorObject.Message = error.Error()
-				respondWithError(w, http.StatusUnauthorized, errorObject)
-				return
-			}
-
-			if token.Valid {
-				next.ServeHTTP(w, r)
-			} else {
-				errorObject.Message = error.Error()
-				respondWithError(w, http.StatusUnauthorized, errorObject)
-				return
-			}
-		} else {
-			errorObject.Message = "Invalid Token"
-			respondWithError(w, http.StatusUnauthorized, errorObject)
-			return
-		}
-	})
-
+	fmt.Fprintf(w, "Hello cloud stack native")
 }
